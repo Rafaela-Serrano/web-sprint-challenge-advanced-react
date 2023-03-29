f5d97ee51b47ad2b5f57998870166bc075f49e55
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2023-03-29 12:59:50 -0500

    try another folder for user event

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/jest-dom';
 import AppFunctional from './AppFunctional';
 
 
@@ -75,5 +75,5 @@ test ("typing on the input results in it's value changing to the entered text", 
     expect(emailDisplay).toBeInTheDocument();
 
   })
-  
+
 })
